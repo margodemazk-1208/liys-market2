d87e1a1913a867a8e3c9e315913efe8a68610d36
margodemazk <margodemazk@gmail.com>
2026-04-15 08:30:31 +0700

    try fix icon 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liy's Market | Solusi Kebutuhan Digital Premium</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('asset/img/logo/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('asset/img/logo/favicon.ico') }}?v=1.1">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <style>
